ac1f92c96fc4f1a8fbd77a8c40a2755e968ed1d1
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-06-15 03:48:54 -0700

    corrected

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,13 +3,14 @@
 Module for rotating a 2D matrix 90 degrees clockwise
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotate the given n x n 2D matrix 90 degrees clockwise in place.
-    
+
     Args:
         matrix (list of list of int): 2D matrix to be rotated
-    
+
     Returns:
         None
     """
